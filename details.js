33905f8894ebc57a1abc497c1cb058db68679e37
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2021-01-15 04:27:47 +0400

    fix error handling in details.js

--- details.js
+++ details.js
@@ -1,28 +1,28 @@
 import { key, ListTypes, fetch, Result, error } from './utils.js'
 
 const urlParams = new URLSearchParams(window.location.search)
-const listData  = urlParams.get('listType')
-const id        = urlParams.get('id')
 
-const listType  = key(urlParams.get('listType'))
-
-if (ListTypes.hasOwnProperty(listType)) { // validate listType
-
-    const value = ListTypes[listType]
-    const path  = `${value.Value}/${id}`
-
-    fetch(path, (result, data) => {
-        switch (result) {
-            case Result.Success:
-                append(value.Details(data))
-                break
-            case Result.Failure:
-                error(data)
-                break
-        }
-    })
-
-} else error('invalid listType parameter')
+const id = urlParams.get('id')
+if (id != null) { // validate id
+    const listType  = key(urlParams.get('listType'))
+    if (ListTypes.hasOwnProperty(listType)) { // validate listType
+    
+        const value = ListTypes[listType]
+        const path  = `${value.Value}/${id}`
+    
+        fetch(path, (result, data) => {
+            switch (result) {
+                case Result.Success:
+                    append(value.Details(data))
+                    break
+                case Result.Failure:
+                    error(data)
+                    break
+            }
+        })
+    
+    } else error('invalid listType parameter')
+} else error('id not specified')
 
 // appends child to container
 function append(entity) {
